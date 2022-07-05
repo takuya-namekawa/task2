9208630a587a86e1f0142a4ac22ecad577ee504c
EC2 Default User <ec2-user@ip-172-31-21-250.us-west-1.compute.internal>
2022-07-05 12:27:21 +0000

    Progress

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,6 +6,7 @@ class BooksController < ApplicationController
   def create
     book = Book.new(book_params)
     book.save
+
     redirect_to book_path(book.id)
   end
 
@@ -14,10 +15,23 @@ class BooksController < ApplicationController
   end
 
   def show
-   @book = Book.find(params[:id])
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books/index'
   end
 
 
